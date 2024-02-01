ab21fb26ac56309de2abedd1b11b16da8274b518
mickylele <158439456+mickylele@users.noreply.github.com>
2024-02-01 15:24:15 +0900

    u
    u

---
+++ src/ct01/Sample1.java
@@ -0,0 +1,18 @@
+package ct01;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+
+		// TODO 自動生成されたメソッド・スタブ
+		double num1 = 12.34;
+		int convertedNum1 = (int) num1;
+		System.out.println("入力:" + num1 + "出力:" + convertedNum1);
+
+		double num2 = 1.88;
+		int convertedNum2 = (int) num2;
+		System.out.println("入力:" + num2 + "出力:" + convertedNum2);
+
+	}
+
+}
